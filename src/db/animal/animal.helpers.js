92a62d1af391057c95c2780e6a504af620511e4a
humdrum <jjrajani@gmail.com>
2017-07-12 20:28:58 -0400

    code comment

--- src/db/animal/animal.helpers.js
+++ src/db/animal/animal.helpers.js
@@ -29,7 +29,7 @@ const calcAge = (birthDate, deathDate) => {
   let birthday = new Date(birthDate);
   let deathday = new Date(deathDate);
 
-
+  /* if age is a string then the animal is still living */
   age = (typeof deathDate === "string") ?
     calcLivingAge(birthday) /* if animal is still living */
   : calcDeceasedAge(birthday, deathday); /* if animal has died */
