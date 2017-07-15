7b5bb465677d2af094d2f63d4e72d6e1da62a1e4
humdrum <jjrajani@gmail.com>
2017-07-15 08:57:46 -0400

    typo

--- src/db/animal/animals.js
+++ src/db/animal/animals.js
@@ -7,7 +7,7 @@ const Animals = [
   new Animal(['Bailey',     'Dog',      new Date(2017, 3, 11), null ]),
   new Animal(['Daisy',      'Dog',      new Date(2010, 2, 20), null ]),
   new Animal(['Eeyore',     'Donkey',   new Date(1926, 9, 14), null ]),
-  new Animal(['Dubo',       'Elephant', new Date(1941, 9, 23), null ]),
+  new Animal(['Dumbo',       'Elephant', new Date(1941, 9, 23), null ]),
   new Animal(['Donika',     'Human',    new Date(1985, 9, 25), null ]),
   new Animal(['Jenna',      'Human',    new Date(1989, 0, 30), null ]),
   new Animal(['Shrek',      'Ogre',     new Date(2001, 3, 22), null ]),
